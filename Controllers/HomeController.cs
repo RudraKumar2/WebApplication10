dee429a4e291be4521caf7b5c4c1f5fede219546
RudraKumar2 <rudrapatel1219@gmail.com>
2024-07-17 11:21:03 -0400

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Postgres.Data;
+
+using ReadDB.Models;
+namespace ReadDB.Controllers;
+public class HomeController : Controller
+{
+    private readonly DataAccess dataAccess;
+    public HomeController(DataAccess dataAccess)
+    {
+        this.dataAccess = dataAccess;
+    }
+    public IActionResult Index()
+    {
+        List<YourModel> data = dataAccess.GetData();
+        return View(data);
+    }
+}
---
+++ Data/DataAccess.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using Npgsql;
+using Microsoft.Extensions.Configuration;
+using WebApplication10.Models;
+using ReadDB.Models;
+
+namespace Postgres.Data
+{
+    public class DataAccess
+    {
+        private readonly string _connectionString;
+
+        public DataAccess(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+
+        public List<YourModel> GetData()
+        {
+            List<YourModel> dataList = new List<YourModel>();
+
+            using (NpgsqlConnection conn = new NpgsqlConnection(_connectionString))
+            {
+                conn.Open();
+                using (NpgsqlCommand cmd = new NpgsqlCommand("SELECT pid, productname, price, productid FROM prices", conn))
+                {
+                    using (NpgsqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            YourModel data = new YourModel
+                            {
+                                pid = reader.GetInt32(reader.GetOrdinal("pid")),
+                                productname = reader.GetString(reader.GetOrdinal("productname")),
+                                price = reader.GetInt32(reader.GetOrdinal("price")),
+                                productid = reader.GetInt32(reader.GetOrdinal("productid"))
+                            };
+                            dataList.Add(data);
+                        }
+                    }
+                }
+            }
+            return dataList;
+        }
+    }
+}
